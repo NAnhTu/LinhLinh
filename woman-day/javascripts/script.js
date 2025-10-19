fd82f9e8b0eb28df020d804c0f1929218a31fcb9
Tu.na <tuna061299@gmail.com>
2025-10-19 14:14:28 +0200

    Update style 20/10

--- woman-day/javascripts/script.js
+++ woman-day/javascripts/script.js
@@ -1,6 +1,7 @@
 onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
+    document.querySelector('.ramo').classList.add('ramo--visible');
     clearTimeout(c);
   }, 1000);
 };
